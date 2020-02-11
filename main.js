7412a5d11733e973cc64faff0233e4ab1321cfa0
Hisstori <browncryan@outlook.com>
2020-02-11 15:29:36 -0500

    Various minor tweeks, major change added js variables.

---
+++ main.js
@@ -0,0 +1,18 @@
+let how = document.querySelector('#how');
+console.log(how);
+let current = document.querySelector('#current');
+console.log(current);
+let high = document.querySelector('#high');
+console.log(high);
+let green = document.querySelector('#green');
+console.log(green);
+let red = document.querySelector('#red');
+console.log(red);
+let yellow = document.querySelector('#yellow');
+console.log(yellow);
+let blue = document.querySelector('#blue');
+console.log(blue);
+let start = document.querySelector('#start');
+console.log(start);
+let reset = document.querySelector('#reset');
+console.log(reset);
