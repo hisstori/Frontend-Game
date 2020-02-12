720b57a5b5db8f124a252322e26f063cedc40e81
Hisstori <browncryan@outlook.com>
2020-02-12 11:21:19 -0500

    cleaned up CSS, HTML, added basic JS functionality

--- main.js
+++ main.js
@@ -1,23 +1,87 @@
-let how = document.querySelector('#how');
+//Create variables for javascript.
+//Have system randomly select colors to play.
+//Colors need to light up on each successful press. (SOUND TO COME AFTER)
+//Create click event for all buttons.
+//Create click event for all simon buttons.
+//Impliment round system to keep track of current rounds and hightest round reached.
+//Add functionality to 'how to', start button, reset button.
+//==============================================
+//==============================================
+
+let order = [];
+console.log(order)
+let playerOrder = [];
+let round 
+let good
+let cpuTurn
+let intervalId
+let sound = true;
+let win 
+
+const howButton = document.querySelector('#how')
 console.log(how);
-let current = document.querySelector('#current');
+const currentRound = document.querySelector('#current');
 console.log(current);
-let high = document.querySelector('#high');
+const highRound = document.querySelector('#high');
 console.log(high);
-let green = document.querySelector('#green');
+const green = document.querySelector('#green');
 console.log(green);
-let red = document.querySelector('#red');
+const red = document.querySelector('#red');
 console.log(red);
-let yellow = document.querySelector('#yellow');
+const yellow = document.querySelector('#yellow');
 console.log(yellow);
-let blue = document.querySelector('#blue');
+const blue = document.querySelector('#blue');
 console.log(blue);
-let start = document.querySelector('#start');
+const startButton = document.querySelector('#start');
 console.log(start);
-let reset = document.querySelector('#reset');
+const resetButton = document.querySelector('#reset');
 console.log(reset);
-let simon = document.querySelector('.simon');
+let simon = document.querySelectorAll('.simon');
 console.log(simon);
-for (let i = 0; i < simon.length; i++) {
-	// console.log(simon[i])
+
+
+currentRound.innerHTML = '--';
+highRound.innerHTML = '--';
+
+// clearColor()
+// clearInterval(intervalID)
+
+// startButton.addEventListener('click', (event) =>)
+
+function play() {
+	win = false;
+	order = [];
+	playerOrder = [];
+	flash = 0;
+	intervalId = 0;
+	round = 1;
+	currentRound.innerHTML = 1;
+	good = true;
+	for (var i = 0; i < 20; i++) {
+		order.push(Math.floor(Math.random() * 4) +1);
+	}
+	cpuTurn = true;
+	intervalId = setInterval(turns), 800);
+	console.log(order)
+}
+
+function turns () {
+	startButton = false;
+
+	if (flash == turn) {
+		clearInterval(intervalId);
+		cpuTurn = false;
+		clearColor();
+		on = true;
+	}
+
+	if (cpuTurn) {
+	  clearColor();
+	  setTimeout(() => {
+	  	if (order[flash] == 1) one();
+	  	if (order[flash] == 1) one();
+	  	if (order[flash] == 1) one();
+	  	if (order[flash] == 1) one();
+	  }, one();
+	}
 }
